a406e960ed169ba02e9a878221c52daa71382b25
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-10-18 15:28:22 +0530

    build.sh

--- build-script/build.sh
+++ build-script/build.sh
@@ -1,1 +1,17 @@
-
+#!/bin/bash
+mkdir -p /opt/ssfs/customization
+cp -r ../* /opt/ssfs/customization 
+chmod 777 -R /opt/ssfs/customization
+chown omsuser:omsuser -R /opt/ssfs/customization
+sudo -E -u omsuser /bin/bash
+source ~/.bashrc
+sudo cat $PUSH_DOCKERCFG_PATH/.dockerconfigjson > /tmp/.dockercfg
+cp -f /opt/ssfs/customization/resources/ydkresources/ydkprefs_export.xml /opt/ssfs/runtime/resources/ydkresources/ydkprefs.xml
+cp -f /opt/ssfs/customization/bin/cdtshell.sh_export.in /opt/ssfs/runtime/bin/cdtshell.sh.in
+echo "ydkprefs.xml and cdtshell.sh.in are updated"
+cd /opt/ssfs/runtime/bin
+./setupfiles.sh
+echo "setupfiles execution completed"
+./cdtshell.sh
+echo "CDT export completed"
+sleep 100
