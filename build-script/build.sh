f7f08353d2b561ffbd1743fac58a534946e6f091
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-10-19 22:13:28 +0530

    Update build.sh

--- build-script/build.sh
+++ build-script/build.sh
@@ -7,15 +7,23 @@ chown omsuser:omsuser -R /opt/ssfs/customization
 sudo -E -u omsuser /bin/bash
 whoami
 cd /opt/ssfs/sources
+cat /var/run/secrets/openshift.io/source/username
+cat /var/run/secrets/openshift.io/source/password
+export GIT_USER="codersyacht"
+export GIT_PASS="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"
+
 cp ./properties/system_overrides.properties ./CDTData/system_overrides.properties
+
 git config --global --replace-all user.name "codersyacht"
 git config --global --replace-all user.email "codersyacht@gmail.com"
-git config --global --replace-all remote.origin.url "https://github.com/codersyacht/oms-cdt-build.git"
+config --global user.password "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"
+git config --global github.token "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"
+
+git remote set-url origin git+ssh://git@github.com/codersyacht/oms-cdt-build.git
 git add *
 git commit -m "Comment V1"
 git branch -M main
-cat /var/run/secrets/openshift.io/source/username
-cat /var/run/secrets/openshift.io/source/password
+
 git push -u origin main --force
 echo "Export completed"
 sleep 50000
