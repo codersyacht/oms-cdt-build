94ba7bd8a5fec07936463a2ee87b3aff87f7853c
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-10-19 08:00:09 +0530

    build.sh

--- build-script/build.sh
+++ build-script/build.sh
@@ -5,18 +5,6 @@ cp -r ../* /opt/ssfs/customization
 chmod 777 -R /opt/ssfs/customization
 chown omsuser:omsuser -R /opt/ssfs/customization
 sudo -E -u omsuser /bin/bash
-source ~/.bashrc
-sudo cat $PUSH_DOCKERCFG_PATH/.dockerconfigjson > /tmp/.dockercfg
-cp -f /opt/ssfs/customization/resources/ydkresources/ydkprefs_export.xml /opt/ssfs/runtime/resources/ydkresources/ydkprefs.xml
-cp -f /opt/ssfs/customization/bin/cdtshell.sh_export.in /opt/ssfs/runtime/bin/cdtshell.sh.in
-cp -f /opt/ssfs/customization/properties/system_overrides.properties /opt/ssfs/runtime/properties/system_overrides.properties
-echo "ydkprefs.xml, cdtshell.sh.in and system_overrides.properties are updated"
-cd /opt/ssfs/runtime/bin
-./setupfiles.sh
-echo "setupfiles execution completed"
-./cdtshell.sh
-echo "CDT export completed"
-EOF
 whoami
 cd /opt/ssfs/sources
 git config --global --replace-all user.name "codersyacht"
@@ -26,7 +14,7 @@ git commit -m "Comment V1"
 git branch -M main
 cat cat /var/run/secrets/openshift.io/source/username
 cat /var/run/secrets/openshift.io/source/password
-git push -u origin main
+git push -u origin main --force
 echo "Export completed"
 sleep 500
 
