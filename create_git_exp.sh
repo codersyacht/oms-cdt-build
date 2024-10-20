6f6f13c5f522f82848d8fbd47e9564924b13050b
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-10-20 10:42:59 +0530

    Create create_git_exp.sh

---
+++ create_git_exp.sh
@@ -0,0 +1,15 @@
+cat > /user/bin/exp << EOF
+
+#!/usr/bin/expect
+
+set timeout 20
+
+set cmd [lrange $argv 1 end]
+set password [lindex $argv 0]
+
+eval spawn $cmd
+expect "Password for 'https://${GIT_USER}@github.com':"
+send "$password\r";
+interact
+
+EOF
