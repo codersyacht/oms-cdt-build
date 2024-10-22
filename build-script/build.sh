25c3517e4eac6fb93666ff0d2d4da8cf7971ce84
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-10-22 20:08:44 +0530

    build.sh

--- build-script/build.sh
+++ build-script/build.sh
@@ -27,6 +27,7 @@ export GIT_USER=$(cat /var/run/secrets/openshift.io/source/username)
 export GIT_PASS=$(cat /var/run/secrets/openshift.io/source/password)
 
 git config --global --replace-all user.name ${GIT_USER}
+git config --global --replace-all user.email ${GIT_USER}@gmail.com
 git remote set-url origin https://${GIT_USER}@github.com/codersyacht/oms-cdt-build.git
 git add *
 git commit -m "Comment V1"
