2c44f244fa95ce738d1034ab129e74b1afad61c5
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-10-22 20:42:25 +0530

    Create test.sh

---
+++ build-script/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+pwd
+
+sudo -E -u omsuser /bin/bash
+source ~/.bashrc
+whoami
+cd /opt/ssfs/sources
+
+export GIT_USER=$(cat /var/run/secrets/openshift.io/source/username)
+export GIT_PASS=$(cat /var/run/secrets/openshift.io/source/password)
+
+git config --global --replace-all user.name ${GIT_USER}
+git config --global --replace-all user.email ${GIT_USER}@gmail.com
+git remote set-url origin https://${GIT_USER}@github.com/codersyacht/oms-cdt-build.git
+git add *
+git commit -m "Comment V1"
+gitexp ${GIT_PASS} git push -u origin main --force
+sleep 50000
