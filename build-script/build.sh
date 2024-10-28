b6c2cd87ce7c319d0990dc1f530c4b19ca4c3d59
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-10-28 14:45:23 +0530

    build.sh

--- build-script/build.sh
+++ build-script/build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+echo ${processtype}
+
+if [${processtype} -eq "cdtimport"]
+then
 echo "Triggering CDT Import"
 chmod 777 ./build-import.sh
 ./build-import.sh
+else
+echo "Triggering CDT Export"
+chmod 777 ./build-export.sh
+./build-export.sh
+fi
