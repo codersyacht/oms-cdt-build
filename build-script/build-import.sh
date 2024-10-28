729d27c8a1cf0699a22fdd5677eb4717192d009d
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-10-28 14:21:16 +0530

    build-import.sh

--- build-script/build-import.sh
+++ build-script/build-import.sh
@@ -1,1 +1,25 @@
-
+#!/bin/bash
+pwd
+yum install expect -y
+yum install zip -y
+cd CDTData
+unzip CDTData-2024-10-28-08:37:50.zip
+mkdir -p /opt/ssfs/customization
+cp -r ../* /opt/ssfs/customization 
+chmod 777 -R /opt/ssfs/customization
+chown omsuser:omsuser -R /opt/ssfs/customization
+rm -rf /opt/ssfs/customization/CDTData
+mv /opt/ssfs/sources/CDTData/CDTData /opt/ssfs/customization/
+sudo -E -u omsuser /bin/bash
+source ~/.bashrc
+sudo cat $PUSH_DOCKERCFG_PATH/.dockerconfigjson > /tmp/.dockercfg
+cp -f /opt/ssfs/customization/resources/ydkresources/ydkprefs_import.xml /opt/ssfs/runtime/resources/ydkresources/ydkprefs.xml
+cp -f /opt/ssfs/customization/bin/cdtshell.sh_export.in /opt/ssfs/runtime/bin/cdtshell.sh.in
+cp -f /opt/ssfs/customization/properties/system_overrides.properties /opt/ssfs/runtime/properties/system_overrides.properties
+echo "ydkprefs.xml, cdtshell.sh.in and system_overrides.properties are updated"
+cd /opt/ssfs/runtime/bin
+./setupfiles.sh
+echo "setupfiles execution completed"
+./cdtshell.sh
+echo "CDT import completed"
+sleep 50000
